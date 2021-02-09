3ce861c7560b7d56d089b15c106756432dfb0fd0
adelbenhamadi <benhamadi.adel@gmail.com>
2021-02-09 12:37:10 +0100

    fix and refactoring

--- app/main.cpp
+++ app/main.cpp
@@ -11,224 +11,293 @@
 #include "../engine/solution.h"
 #include "../engine/engine.h"
 
+
 using namespace std;
 
+#define VERSION "0.2"
+#define VERSION_SUFFIX "rev225"
+#define USE_LIB 0
 
-
-void getTimeStr(char *buf)
-{
-    time_t t=time(0);
-    struct tm  *now=localtime(&t);
-    strftime( buf, 128,"time%d%m%y-%H%M%S", now );
+void getTimeStr(char* buf) {
+    time_t t = time(0);
+    struct tm* now = localtime(&t);
+    strftime(buf, 128, "time%d%m%y-%H%M%S", now);
 }
-
-int main(int argc, char *argv[], char *envp[])
+const char* get_version(void)
 {
-    char *pm_configfile;//[254]="lycee-sec-siliana-final-mixte1_uncompressed.edt";
-    char pm_datadir[255] ="c:\\solution\\out\\";//ExtractFilePath();
-    char str1[255],str2[128];
-    double hltx=95.0;
-    double lltx=85.0;
-    double mltx=90.0;
-    bool justCheck=false;
+    return VERSION "-" VERSION_SUFFIX;
+}
+void printversion(void) {
+    printf("\n");
+    printf("Scheduler Engine %s\n", get_version());
+    printf("(C) 2012-2021 Adel BEN HAMADI <benhamadi.adel@gmail.com>\n\n");
 
-    if (argc >= 3)
+}
+void printhelp(void)
+{
+
+    printf("\n");
+    printf("Scheduler Engine %s\n", get_version());
+
+    printf("\t--help                -h             Print this help screen\n");
+    printf("\t--version             -V             Print version number\n");
+    printf("\t--debug               -D             Enable engine debug messages\n");
+    printf("\t--quiet               -Q             Only output error messages\n");
+    printf("\t--stdout                             Write to stdout instead of stderr\n");
+    printf("\t--no-summary                         Disable summary at end of searching\n");
+    printf("\t--verbose            -v              verbose some info!\n");
+
+    printf("\n");
+    //  printf("(*) Default settings\n");
+
+}
+#if USE_LIB
+bool loadLibrary(HINSTANCE libDLL, I_Engine_Intf** pInterface) {
+    GETINTERFACE    pfnGetIntf = 0; //pointer to GetMyInterface function
+    //I_Engine_Intf * pInterface  =0;
+
+    if (libDLL != NULL)
     {
+        //Get the functions address
+        pfnGetIntf = (GETINTERFACE)::GetProcAddress(libDLL, "GetEngineIntf");
 
-        if(_stricmp( argv[1], "-f" ) == 0 )
-            pm_configfile=argv[2];
-        if((argc>=4)&&(_stricmp( argv[3], "-c" ) == 0 )){
-            justCheck=true;
+        //Release Dll if we werent able to get the function
+        if (pfnGetIntf == 0)
+        {
+            ::FreeLibrary(libDLL);
+            return false;
         }
-         else{
-            if((argc>=5)&&(_stricmp( argv[3], "-hl")==0))
-                hltx=atof(argv[4]);
-            if((argc>=7)&&(_stricmp( argv[5], "-ll")==0))
-                lltx=atof(argv[6]);
-            if((argc>=9)&&(_stricmp( argv[7], "-ml")==0))
-                mltx=atof(argv[8]);
-       }
-    } ;
 
+        //Call the Function
+        HRESULT hr = pfnGetIntf(pInterface);
 
+        //Release if it didnt work
+        if (FAILED(hr))
+        {
+            ::FreeLibrary(libDLL);
+            return false;
+        }
 
+        return true;
+    }
+    return false;
+    }
+void freeLibrary(HINSTANCE libDLL, I_Engine_Intf** pInterface) {
+    //How to release the interface
 
+    FREEINTERFACE pfnFreeIntf = (FREEINTERFACE)::GetProcAddress(libDLL, "FreeEngineIntf");
+    if (pfnFreeIntf != 0)
+        pfnFreeIntf(pInterface);
+
+    //Release the DLL if we dont have any use for it now
+    ::FreeLibrary(libDLL);
+
+}
+#endif
+
+int main(int argc, char* argv[], char* envp[])
+{
+
+    char pm_datadir[255] = "./out";
+    char str1[255], str2[128];
+
+    CEngineConfig engine_cfg;
+ 
+
+    if (argc == 2) {
+        if ((_stricmp(argv[1], "-h") == 0) || (_stricmp(argv[1], "--help") == 0)) {
+            printhelp();
+            return 0;
+        }
+        if ((_stricmp(argv[1], "-V") == 0) || (_stricmp(argv[1], "--version") == 0)) {
+            printversion();
+            return 0;
+        }
+    }
+    engine_cfg.slowprinting = true;
+    engine_cfg.optimizebranching = false;
+
+    int iarg;
+    for (iarg = 1; iarg < argc - 1; iarg++) {
+        if (_stricmp(argv[iarg], "-f") == 0)
+            engine_cfg.configfile = argv[iarg + 1];
+        else if (_stricmp(argv[iarg], "-hl") == 0)
+            engine_cfg.hltx = atof(argv[iarg + 1]);
+        else if (_stricmp(argv[iarg], "-ll") == 0)
+            engine_cfg.lltx = atof(argv[iarg + 1]);
+        else if (_stricmp(argv[iarg], "-ml") == 0)
+            engine_cfg.mltx = atof(argv[iarg + 1]);
+        else if (_stricmp(argv[iarg], "--slowprinting") == 0)
+            engine_cfg.slowprinting = true;
+        else if (_stricmp(argv[iarg], "--optimizebranching") == 0)
+            engine_cfg.optimizebranching = true;
+
+    }
+    if (!engine_cfg.configfile) {
+        printf("No Config file provided!\n");
+        exit(0);
+    }
     Console::initConsoleScreen();
     Console::HighVideo();
     Console::Clear();
     Console::SetTitle("Testing console!");
     Console::SetFGColor(ColorWhite);
 
-    int ecount=0;
+    
+    int ecount = 0;
+    /*
     printf("\nPress any key to cancel ");
-    while(!Console::KeyDown() && ecount<5)
-    {
+    while (!Console::KeyDown() && ecount < 5) {
         Console::Write(".");
-        Sleep(100);
+        Sleep(500);
 
         ecount++;
-    } ;
+    };
 
-    if (ecount<5)  exit(0);
-
+    if (ecount < 5)  exit(0);
+    Beep(750, 175);
+    */
     Console::Clear();
-    Console::WriteEx("Gemp Generetor v1.0\n",ColorYellow,ColorBlue);
-    printf("Copyright 2012 Adel BEN HAMADI. All rights reserved\n\n");
-
-    printf("-------------------------------------------------------\n");
-    printf("VERSION:         1.0.0.0 rev 000\n");
+    Console::WriteEx("Scheduler Engine \n", ColorBlack, ColorCyan);
+    printf("(c) 2012-2021 Adel BEN HAMADI <benhamadi.adel@gmail.com>\n");
+    printf("---------------------------------------------------------\n");
+    printf("VERSION:         %s\n", get_version());
     printf("DEBUG MODE:      ");
 #if ENGINE_DEBUGMODE_LEVEL > 0
-    Console::WriteEx("enabled\n",ColorWhite,ColorRed);
+    Console::WriteEx("enabled\n", ColorBlack, ColorRed);
 #else
-    Console::WriteEx("disabled\n",ColorSilver);
+    Console::WriteEx("disabled\n", ColorSilver);
 #endif
+
     //load engine
+    printf("ENGINE:          %s\n", ENGINENAME);
+    Console::SetCursorPosition(3, Console::GetCursorY() + 1);
+#if USE_LIB
+    I_Engine_Intf engine ;
+    HINSTANCE hEngineLib = ::LoadLibrary("libEngine.dll");
+    try {
+        if (loadLibrary(hEngineLib, engine) != true) {
+            perror("\nUnable to load libraray interface from 'libEngine.dll'");
+            exit(-1);
+        }
 
-
-    printf("ENGINE:          %s\n",ENGINENAME);
-    printf("CONFIG FILE:     %s\n",pm_configfile);
-    printf("-------------------------------------------------------\n");
-
-    Console::SetCursorPosition(3,Console::GetCursorY()+1);
-    emEngine* engine;
-    // engine=new emEngine(pm_configfile);
+        if (engine.load(engine_cfg))
+            printf("LOAD CONFIG FILE:     %s..[ok]\n", engine.config().configfile);
+        else
+        {
+            printf("LOAD CONFIG FILE:     %s..[failed]\n", engine.config().configfile);
+            perror("\nUnable to load config file");
+            exit(-1);
+        }
+    }
+    catch (...) {
+        exit(-1);
+    }
+#else
+    emEngine engine;
+   
     try
     {
 
-        engine=new emEngine(pm_configfile);
-
+        if (engine.load(engine_cfg))
+            printf("LOAD CONFIG FILE:     %s\n", engine.config().configfile);
+        else
+        {
+            printf("LOAD CONFIG FILE:     %s..[failed]\n", engine.config().configfile);
+            perror("\nUnable to load config file");
+            exit(-1);
+        }
     }
-    catch(...)
+    catch (...)
     {
         exit(ENGINE_ERROR_LOADING);
-
     }
-    if(engine==NULL)
-        exit(ENGINE_ERROR_LOADING);
-    int valp,valc,osp,osc;
-    int multi=15;
-    int oldvalc=10*multi;
-    int oldvalp=10*multi;
-    int oldosp=10*multi;
-    int oldosc=10*multi;
+#endif
+
+    printf("ENGINE:          v%d\n", engine.pluginInfo().version_maj);
+    printf("-----------------------------------------------------------");
+
+    //start searching
+    Console::SetCursorPosition(3, Console::GetCursorY());
+
+
+    int valp, valc, osp, osc;
+    int multi = 15;
+    int oldvalc = 10 * multi;
+    int oldvalp = 10 * multi;
+    int oldosp = 10 * multi;
+    int oldosc = 10 * multi;
     double val;
-    double oldval=oldvalp+oldosp+oldvalc+oldosc;
+    double oldval = oldvalp + oldosp + oldvalc + oldosc;
 
-    double a_percent=0;
-    double max_percent=0;
+    double a_percent = 0;
+    double max_percent = 0;
 
-    ecount=1;
-    int sol=1;
-    int notaccepted=0;
-    int sol_retenu=0;
-
-    if(justCheck==true){
-        Console::SetCursorPosition(4,Console::GetCursorY()+1);
+    ecount = 1;
+    int sol = 1;
+    int notaccepted = 0;
+    int sol_retenu = 0;
+    while (sol < 1000000) {
+        Console::SetCursorPosition(4, Console::GetCursorY() + 1);
         Console::Write("\n");
-        Console::Write("Entering checking mode...\n");
-        Console::Write("Checking solution:  ");
-        Console::WriteLine(pm_configfile);
-        engine->solution.rebuildSolution();
-        printf("rebuild solution..[ok]\n\n");
-        engine->solution.getOptimizeValue(&valp,&valc,&osp,&osc);
-        wsprintf(str1,"->solution optimize info:%d %d/%d/%d/%d\n",sol,valp,valc,osp,osc);
+        Console::Write("---------------------\n");
+        printf(" Solution:%d  P:%.2f%%\n", sol, max_percent);
+
+        Console::Write("---------------------\n");
+        engine.execute(sol == 1,/*(sol_retenu=0)or(sol-sol_retenu > 5)*/sol % 3 == 1);
+        // Sleep(250);
+
+        //Console::SetCursorPosition(6,Console::GetCursorY()+1);
+        Console::Write("\n");
+        //engine.rebuildSolution();
+        engine.getOptimizeValue(&valp, &valc, &osp, &osc);
+
+        sprintf_s(str1, "\t->solution found:%d %d/%d/%d/%d\n", sol, valp, valc, osp, osc);
         Console::Write(str1);
 
-
-        if (engine->solution.verifyProcessedSeances(false)==false)
-            {
-                Console::WriteEx("->invalid solution!\n",ColorRed);
-            }
-            else
-            {
-
-                Console::WriteEx("->this is a valid solution!\n",ColorGreen) ;
-
-                val=valp+osp/*+valc+osc*/;
-                a_percent=(double) (100*(1-(val/engine->solution.SeanceTableCount()))) ;
-                printf("->optimized at %.2f%% P:%d/%d C:%d/%d\n",a_percent,valp,osp,valc,osc);
-            }
-        printf("\nPress any key to exit ");
-        while(true){
-           if (Console::KeyDown())
-            exit(0);
+        if (engine.verifySolution(false) == false) {
+            notaccepted++;
+            Console::WriteEx("\t->solution not accepted!\n", ColorRed);
         }
-
-    }
-
-        while (sol<1000000)
+        else
         {
-            Console::SetCursorPosition(4,Console::GetCursorY()+1);
-            Console::Write("\n");
-            Console::Write("---------------------\n");
-            printf(" Solution:%d  P:%.2f%%\n",sol,max_percent);
 
-            Console::Write("---------------------\n");
-            engine->execute(sol==1,/*(sol_retenu=0)or(sol-sol_retenu > 5)*/sol % 3==1,hltx,lltx,mltx);
-            // Sleep(250);
+            sprintf_s(str1, "\t->solution accepted[%d/%d]\n", sol - notaccepted, sol);
+            Console::WriteEx(str1, ColorGreen);
 
-            Console::Write("\n");
-            //engine->solution.rebuildSolution();
-            engine->solution.getOptimizeValue(&valp,&valc,&osp,&osc);
-            wsprintf(str1,"  ->solution found:%d %d/%d/%d/%d\n",sol,valp,valc,osp,osc);
-            Console::Write(str1);
 
-            if (engine->solution.verifyProcessedSeances(false)==false)
-            {
-                notaccepted++;
-                Console::WriteEx("  ->solution not accepted!\n",ColorRed);
+            val = valp + osp/*+valc+osc*/;
+            a_percent = (double)(100 * (1 - (val / engine.shiftsCount())));
+            printf("\t->optimized at %.2f%% P:%d/%d C:%d/%d\n", a_percent, valp, osp, valc, osc);
+
+            if (val < oldval) {
+                sol_retenu = sol;
+                max_percent = a_percent;
+                oldval = val;
+                oldvalp = valp;
+                oldvalc = valc;
+                oldosp = osp;
+                oldosc = osc;
+                printf("\t->solution autosaved P:%d/%d C:%d/%d\n", valp, osp, valc, osc);
+
+
+                getTimeStr(str2);
+                sprintf_s(str1, "sol-v%d-%d-%d-%s.gedt", engine.pluginInfo().version_maj, valp, valc, str2);
+                engine.save(str1);
+                MessageBeep(0xFFFFFFFF);// MB_ICONASTERISK MB_ICONEXCLAMATION MB_ICONOK
+                if (val == 0)
+                    break;
+
             }
-            else
-            {
+        }
+        sol++;
+    }//while
+   
+    engine.release();
 
-                wsprintf(str1,"  ->solution accepted[%d/%d]\n",sol-notaccepted,sol) ;
-                Console::WriteEx(str1,ColorGreen);
-
-
-                val=valp+osp/*+valc+osc*/;
-                a_percent=(double) (100*(1-(val/engine->solution.SeanceTableCount()))) ;
-                printf("  ->optimized at %.2f%% P:%d/%d C:%d/%d\n",a_percent,valp,osp,valc,osc);
-
-                if (val<oldval )
-                {
-                    sol_retenu=sol;
-                    max_percent=a_percent;
-                    oldval= val;
-                    oldvalp=valp;
-                    oldvalc=valc;
-                    oldosp=osp;
-                    oldosc=osc;
-                    printf("  ->solution autosaved P:%d/%d C:%d/%d\n",valp,osp,valc,osc);
-
-
-                    getTimeStr(str2);
-                    wsprintf(str1,"sol-e%s-%d-%d-%s.edt",ENGINENAME,valp,valc,str2);
-                    engine->solution.SaveToFile(str1);
-                    MessageBeep(MB_ICONASTERISK);// MB_ICONASTERISK MB_ICONEXCLAMATION MB_ICONOK
-                    if (val==0)
-                        break;
-
-                }
-            }
-            sol++;
-        }//while
-
-
-    delete engine;
-    exit(0);
-
-    /* CemSolution *sol=new CemSolution();
-     try{
-
-     sol->LoadFromFile("lycee-sec-siliana-final-mixte1_uncompressed_saved.edt");
-     //sol->SaveToFile("lycee-sec-siliana-final-mixte1_uncompressed_saved.edt");
-     }
-     catch( char * err ) {
-       printf("Exception raised: %s error n�:%d\n",err,(int)GetLastError());
-     }
-     delete sol;// sol.~CemSolution();
-     */
-
-
+    //end
+#if USE_LIB
+    freeLibrary(hEngineLib, engine);
+#endif
 }
+
