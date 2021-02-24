e86c3ac4bd32b733f7d801dbf8f3c9084613c99f
adelbenhamadi <benhamadi.adel@gmail.com>
2021-02-24 11:55:21 +0100

    members of CShift: length,mindex,cindex,byGroup,every2weeks and pindex are now readonly

--- engine/classes.h
+++ engine/classes.h
@@ -15,40 +15,40 @@ const unsigned int WORKABLE_DAY_COUNT = 6;
 const unsigned int PARK_DAY_COUNT = 5;
 const unsigned int MAX_DAY_COUNT = WORKABLE_DAY_COUNT + PARK_DAY_COUNT;
 
-typedef int CDayTable[MAX_DAY_COUNT][ HOUR_TICK_COUNT ];
+typedef int CDayTable[MAX_DAY_COUNT][HOUR_TICK_COUNT];
 typedef DWORD CBitDayTable[MAX_DAY_COUNT];
-typedef unsigned int CMapDayHoursTable[MAX_DAY_COUNT][ HOUR_TICK_COUNT ];
-typedef bool CMapDayTable[MAX_MAT_COUNT][ WORKABLE_DAY_COUNT ];
-typedef bool CMapHourTable[MAX_DAY_COUNT][ HOUR_TICK_COUNT ];
+typedef unsigned int CMapDayHoursTable[MAX_DAY_COUNT][HOUR_TICK_COUNT];
+typedef bool CMapDayTable[MAX_MAT_COUNT][WORKABLE_DAY_COUNT];
+typedef bool CMapHourTable[MAX_DAY_COUNT][HOUR_TICK_COUNT];
 
-enum EEncryptMode{ ecmM1, ecmM2 };
-enum ECompressionMode{ crmMode1, crmMode2 };
-enum EDrawMode{ doHighLighted, doLocked, doMarked, doParked };
-enum EFillMode{ foWeekA, foWeekB, foMixte, foNoWhere };
+enum EEncryptMode { ecmM1, ecmM2 };
+enum ECompressionMode { crmMode1, crmMode2 };
+enum EDrawMode { doHighLighted, doLocked, doMarked, doParked };
+enum EFillMode { foWeekA, foWeekB, foMixte, foNoWhere };
 
-enum EOptimizeOption{
+enum EOptimizeOption {
 	ooNoOrphShift, ooNoBreakHour, ooNoHeurePointe,
-	ooNoSportAfterClass, ooPrefMorning, ooPrefEvening, ooNoSportAfter17h,_ooLast
+	ooNoSportAfterClass, ooPrefMorning, ooPrefEvening, ooNoSportAfter17h, _ooLast
 };
 typedef  EOptimizeOption EOptimizeOptions[EOptimizeOption::_ooLast];
 
-enum EShiftOptimizeOption{ soPrefMorning, soPrefEvening, soNoBusyHour };
+enum EShiftOptimizeOption { soPrefMorning, soPrefEvening, soNoBusyHour };
 
-enum EGlobalOptimizeOption{
+enum EGlobalOptimizeOption {
 	goClassOptimize, goProfOptimize, goNotIncludedCroom,
 	goNotIncludedClasse, goNotIncludedProf, goNotIncludedMat,
 	goRandomizeShifts, goAllwaysRandomizeShifts, goNotProcessLockedShifts,
-	goSortByShiftLength,_goLast
+	goSortByShiftLength, _goLast
 };
-typedef  EGlobalOptimizeOption EGlobalOptimizeOptions[EGlobalOptimizeOption::_goLast] ;
+typedef  EGlobalOptimizeOption EGlobalOptimizeOptions[EGlobalOptimizeOption::_goLast];
 
-enum EScheduleMode{ emCroom, emClasse, emProf, emMat };
-enum ELinkType{ ltClasse, ltCroom, ltProf, ltGroup, ltClear };
+enum EScheduleMode { emCroom, emClasse, emProf, emMat };
+enum ELinkType { ltClasse, ltCroom, ltProf, ltGroup, ltClear };
 
 struct OptimizeInfo {
 	int orphanedIdx;
 	int compactIdx;
-	OptimizeInfo(): orphanedIdx(0),compactIdx(0){}
+	OptimizeInfo() : orphanedIdx(0), compactIdx(0) {}
 	OptimizeInfo(const int oi, const int ci) : orphanedIdx(oi), compactIdx(ci) {}
 };
 struct ScheduleConfig {
@@ -86,71 +86,83 @@ struct ScheduleConfig {
 
 };
 
-struct CMat{
+struct CMat {
 	char        symbol[64];
 	char        name[64];
-	int      fdayC1, fdayC2;
+	int         fdayC1, fdayC2;
 	bool        notIncluded;
 
+	
 };
 
 //classe 
-struct CClasse{
-	char    name[9];
+struct CClasse {
+
 	int      rank;
-	int      niveau;
-	int      nb_eleve;
+	char     name[9];
+	int      level;
+	int      reserved0;
 	CDayTable	weeka, weekb;
 	WORD    options;
 	bool        notIncluded;
 	int      attachedClasses[10];
-
+private:
+	
 };
 //prof 
-struct CProf{
+struct CProf {
 	char       symbol[9];
 	char       name[255];
 	int     rank;
 	int     mIndex;
 	CDayTable  weeka, weekb;
 	WORD     options;
-	int         fday;
-	bool        notIncluded;
+	 int         fday;
+	 bool        notIncluded;
 
+	
 };
 
 //shift 
-struct CShift{
+struct CShift {
 	int         rank;
 	int      	hour;// start hour
 	int    		day;//day
-	int       	length;//length
+	const int   length;//length
 	int         delta;
-	int         mindex;
-	int         cindex;
+	const int   mindex;
+	const int   cindex;
 	int         crindex;
 	int         croom_type;
-	int         pindex;
+	const int   pindex;
 	int         groupedwith;
 	int         dogroupwith;
-	int         doAlternatewith; //weekly alternate ..
+	int         fillMode; 
 	WORD		options;//EShiftOptimizeOption;
 
-	bool     every2weeks;
-	bool     bygroup;
+	const bool     every2weeks;
+	const bool     bygroup;
 	WORD     drawOption;
+	CShift():length(0), mindex(-1), cindex(-1), pindex(-1), every2weeks(0), bygroup(0) {};
+	CShift(const int l, const int mati, const int classei, const int profi, const bool e2w, const bool bg) :
+		length(l), mindex(mati), cindex(classei), pindex(profi), every2weeks(e2w), bygroup(bg) {};
+	CShift operator=(const CShift& rh) {
+	 return CShift(rh.length, rh.mindex, rh.cindex, rh.pindex, rh.every2weeks, rh.bygroup);
+}
 };
 //croom 
- struct CCroom{
+struct CCroom {
+	
 	char     name[9];
 	int      rank;
-	int      stype;
+	 int      stype;
 	CDayTable	weeka;
 	CDayTable weekb;
-	bool        notIncluded;
+	 bool        notIncluded;
+
 };
 
- struct CFillInfo{
+struct CFillInfo {
 	int fShift;
 	int fCroom;
 	int fDay, fHour;
@@ -169,7 +181,7 @@ typedef struct {
 	char description[255];
 }CEnginePlugin, * PCEnginePlugin, * LPCEnginePlugin;
 
-struct CEngineConfig{
+struct CEngineConfig {
 	char* configfile;
 	double hltx;
 	double mltx;
